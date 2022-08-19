5c6175c73a41283b505f6f2b2aba0e1d3ef493ac
MariaGee <masha.avramchencko@yandex.com>
2022-08-20 01:45:08 +0300

    Add Example 023

---
+++ Example023/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27     5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите, пожалуйста, число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+double count = 1;
+
+for(int i = 1; i <= number; i++)
+{
+    count = Math.Pow(i, 3);
+    Console.Write(count + "  ");
+}
